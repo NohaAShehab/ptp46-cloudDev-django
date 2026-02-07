003916d83c92d03d94e46127c39c3caf9d8b7c37
Noha Shehab <nohashehab.iti@gmail.com>
2026-02-07 10:20:17 +0200

    serving static files

--- cloud-track/courses/views.py
+++ cloud-track/courses/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render
 
 
 def courses_landing(request):
-    courses =["python", "django", 'Kubernetes', 'jenkins', 'SQL', 'MicroService']
+    # courses =["python", "django", 'Kubernetes', 'jenkins', 'SQL', 'MicroService']
+    courses = [
+        {
+            "id":1, "name":"python", "image":"python.png"
+        },
+        {
+            "id": 2, "name": "Kubernetes", "image": "kubernetes.png"
+        },
+        {
+            "id": 3, "name": "docker", "image": "docker.png"
+        }
+    ]
     return render(request, 'courses/landing.html',
                   context={'courses':courses})
