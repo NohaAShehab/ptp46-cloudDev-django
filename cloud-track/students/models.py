ee84d012d93a50055fabf4f243c70329ae3e8c54
Noha Shehab <nohashehab.iti@gmail.com>
2026-02-07 11:31:06 +0200

    creating first models

--- cloud-track/students/models.py
+++ cloud-track/students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+"""
+    table students
+    id, name, grade, age, email, image, gender
+"""
+"""
+    any table generate via model (framework)
+    default of table fields not null --> unless say it is null 
+"""
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    grade = models.IntegerField(null=True)
+    age = models.IntegerField(default=10)
+    email = models.EmailField(null=True, unique=True, blank=True)
+    image= models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}({self.id})"
